e057a96e4adada46ab169b9e2de79cf699e395a1
Shreejoy Dash <shreejoy@pixysos.com>
2021-08-21 16:11:45 +0530

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,47 +1,35 @@
-const { parse } = require('url');
-const { timingSafeEqual } = require('crypto');
-const { getScreenshot } = require('./chromium');
-const { getInt, getUrlFromPath, isValidUrl } = require('./validator');
+const playwright = require("playwright-aws-lambda")
 
-const SECRET_KEY = process.env.SECRET_KEY;
-
-const compare = (a, b) => {
+module.exports = async (req, res) => {
+    let browser = null
+    const { query } = req
     try {
-        return timingSafeEqual(Buffer.from(a, "utf8"), Buffer.from(b, "utf8"));
-    } catch {
-        return false;
-    }
-};
-
-module.exports = async function (req, res) {
-    console.log(SECRET_KEY, req.query.key, compare(req.query.key, SECRET_KEY));
-    if (!compare(req.query.key, SECRET_KEY)) {
-        res.statusCode = 403;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Bad ?key=</h1><p>Permission denied</p>');
-        return;
-    }
-    try {
-        const { pathname = '/', query = {} } = parse(req.url, true);
-        const { type = 'png', quality, fullPage } = query;
-        const url = getUrlFromPath(pathname);
-        const qual = getInt(quality);
-        let viewportWidth = parseInt(req.query.viewportWidth || '800');
-        let viewportHeight = parseInt(req.query.viewportHeight || '600');
-        if (!isValidUrl(url)) {
-            res.statusCode = 400;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
-        } else {
-            const file = await getScreenshot(url, type, qual, fullPage, viewportWidth, viewportHeight);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', `image/${type}`);
-            res.end(file);
+        if (query.url && isValidUrl(query.url)) {
+            browser = await playwright.launchChromium({ headless: true })
+            const context = await browser.newContext()
+            const page = await context.newPage()
+            await page.goto(query.url)
+            const screenshot = await page.screenshot({ type: "png" })
+            res.setHeader("Content-Type", "image/png")
+            res.status(200).send(screenshot)
+        } else throw "Please provide a valid url"
+    } catch (error) {
+        res.status(500).send({
+            status: "Failed",
+            error,
+        })
+    } finally {
+        if (browser !== null) {
+            await browser.close()
         }
-    } catch (e) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-        console.error(e.message);
     }
-};
+}
+
+function isValidUrl(string) {
+    try {
+        new URL(string)
+    } catch (_) {
+        return false
+    }
+    return true
+}
